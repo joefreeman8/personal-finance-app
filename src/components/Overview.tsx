17bb08268ea5919950346ef4931a6d1496381ea8
joefreeman8 <joefreeman8@gmail.com>
2024-09-20 20:14:14 +0100

    brought in data.json so can access balance data - coded the html & css for all devices for the first part of overview.tsx

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -1,26 +1,36 @@
-
+import { balance } from '../data.json'
 export default function Overview() {
 
-
-
+  const currentBalance = balance.current.toFixed(2)
+  const totalIncome = balance.income.toFixed(2)
+  const totalExpenses = balance.expenses.toFixed(2)
 
   return (
-    <section className="xl:my-400 xl:mx-500">
-      <h1 className="text-preset-1 xl:my-100">
+    <section className="py-300 px-200 lg:py-400 lg:px-500">
+      <h1 className="text-preset-1 mb-400 xl:mb-0 xl:my-100">
         Overview
       </h1>
-      <div className="xl:my-400 xl:flex xl:justify-between">
-        <div className="xl:mr-300 xl:p-300 xl:bg-grey900 xl:text-white xl:w-1/3 xl:rounded-lg">
+      <div className="flex flex-col lg:flex-row lg:my-400 lg:justify-between">
+        <div className="
+          bg-grey900 text-white rounded-lg p-250 mb-150
+          lg:mb-0 lg:p-300 lg:mr-300 lg:w-1/3
+        ">
           <p className="text-preset-4">Current Balance</p>
-          <p className="text-preset-1 xl:mt-150">$4,836.00</p>
+          <p className="text-preset-1 mt-150">${currentBalance}</p>
         </div>
-        <div className="xl:mr-300 xl:p-300 xl:bg-white xl:w-1/3 xl:rounded-lg">
+        <div className="
+          bg-white rounded-lg p-250 mb-150 
+          lg:mb-0 lg:p-300 lg:mr-300 lg:w-1/3
+        ">
           <p className="text-preset-4 text-grey500">Income</p>
-          <p className="text-preset-1 xl:text-grey900 xl:mt-150">$9,000.00</p>
+          <p className="text-preset-1 text-grey900 mt-150">${totalIncome}</p>
         </div>
-        <div className="xl:p-300 xl:bg-white xl:w-1/3 xl:rounded-lg">
+        <div className="
+            bg-white rounded-lg p-250 
+            lg:mb-0 lg:p-300 lg:w-1/3
+            ">
           <p className="text-preset-4 text-grey500">Expenses</p>
-          <p className="text-preset-1 text-grey900 xl:mt-150">$9,000.00</p>
+          <p className="text-preset-1 text-grey900 mt-150">${totalExpenses}</p>
         </div>
       </div>
     </section>
