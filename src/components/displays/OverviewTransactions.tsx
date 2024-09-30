0b83cfb42fa9e39573ab40eba82ea4cc4a8e4a3c
joefreeman8 <joefreeman8@gmail.com>
2024-09-30 16:38:38 +0100

    styling for transaction display on Overview page completed

--- src/components/displays/OverviewTransactions.tsx
+++ src/components/displays/OverviewTransactions.tsx
@@ -15,9 +15,30 @@ interface OverviewTransactionsProps {
 }
 
 export default function OverviewTransactions({ transactions, navigateToTransactions }: OverviewTransactionsProps) {
+
+  function formatDateString(dateString: string): string {
+    return new Date(dateString).toLocaleDateString('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric'
+    })
+  }
+
+  function formatAmount(amount: number): string {
+    if (amount >= 0) {
+      return `+$${amount.toFixed(2)}`
+    } else {
+      return `-$${amount.toFixed(2).toString().slice(1)}`
+    }
+  }
+
+  function styleAmount(amount: number): string {
+    return amount > 0 ? 'text-green' : 'text-grey900'
+  }
+
   return (
-    <div className='p-300 md:p-400 rounded-xl bg-white'>
-      <div className='flex justify-between mb-250'>
+    <div className='mt-300 p-300 md:p-400 rounded-xl bg-white'>
+      <div className='flex justify-between mb-150'>
         <h2 className='text-preset-2 text-grey900'>Transactions</h2>
         <button className='flex items-center text-grey500 text-preset-4'>
           <p onClick={navigateToTransactions} className='mr-150'>View All</p>
@@ -26,25 +47,26 @@ export default function OverviewTransactions({ transactions, navigateToTransacti
       </div>
       <div className='flex flex-col'>
         {transactions && (
-          transactions.map((transaction, index) => {
-            console.log(transaction.avatar)
-            return (
-              index < 6 && (
-                <div key={index} className='w-[130px] flex items-center'>
-                  {/* <div className='w-1 rounded'> */}
-                  {/* <div className='ml-200'> */}
-                  <img src={transaction.avatar} alt='avatar' />
-                  <p className='text-preset-5 text-grey500 mb-50 w-[100px]'>{transaction.name}</p>
-                  <p className='text-preset-4-bold text-grey900'>${transaction.amount.toFixed(2)}</p>
+          transactions.map((transaction, index) => (
+            index < 5 && (
+              <div key={index} className={`${index !== 4 ? 'border-b' : ''}`}>
+                <div className={`flex items-center justify-between w-full ${index !== 4 ? 'my-250' : 'mt-250'}`}>
+                  <div className='flex flex-row items-center'>
+                    <img src={transaction.avatar} alt='avatar' className='rounded-full h-[40px] mr-200' />
+                    <p className='my-100 text-preset-4-bold text-grey900'>{transaction.name}</p>
+                  </div>
+                  <div className='flex flex-col'>
+                    <p className={`flex justify-end text-preset-4-bold mb-100 ${styleAmount(transaction.amount)}`}>
+                      {formatAmount(transaction.amount)}
+                    </p>
+                    <p className='text-grey500 text-preset-5'>{formatDateString(transaction.date)}</p>
+                  </div>
                 </div>
-                // </div>
-                // </div>
-              )
+              </div>
             )
-          })
+          ))
         )}
       </div>
     </div>
-    // </div>
   )
 }
