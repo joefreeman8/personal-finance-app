64a4dceffeffdb38877b29ac26fdd1a1ba07708d
joefreeman8 <joefreeman8@gmail.com>
2025-02-12 10:42:45 +0000

    small tweaks to helper functions

--- src/utilities/formattingFunctions.ts
+++ src/utilities/formattingFunctions.ts
@@ -6,13 +6,23 @@ export function formatDateString(dateString: string): string {
   })
 }
 
+// .toLocaleString provides the comma seperator for larger numbers
 export function formatAmount(amount: number): string {
   if (amount >= 0) {
-    return `+$${amount.toFixed(2)}`
+    return `+$${amount.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    })}`
   } else {
-    return `-$${amount.toFixed(2).toString().slice(1)}`
+    return `-$${amount.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2
+    }).slice(1)}`
   }
 }
+
+
+
 
 export function styleAmount(amount: number): string {
   return amount > 0 ? 'text-green' : 'text-grey900'
